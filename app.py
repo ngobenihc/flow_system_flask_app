3f037e4280de02e89ef7deeaf493c3d3e2972a45
ngobenihc <ngobenihc@yahoo.com>
2024-06-27 11:35:07 +0200

    flask project

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+
+UPLOAD_FOLDER = 'uploads'
+STATIC_FOLDER = 'static'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+if not os.path.exists(STATIC_FOLDER):
+    os.makedirs(STATIC_FOLDER)
 
 
 @app.route('/')
@@ -14,18 +22,30 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
-    if file:
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filepath)
-        data = pd.read_excel(filepath)
-        fig = px.bar(data, x='Month', y=['Income', 'Expenses'])
-        graph = fig.to_html(full_html=False)
-        return render_template('graph_Display.html', graph=graph)
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        dob = request.form['dob']
+        file = request.files['file']
+
+        # Save the file to a temporary location
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
+
+        # Process the file and generate a graph
+        data = pd.read_excel(file_path)
+        plt.figure()
+        data.plot(x='Month', y=['Income', 'Expenses'], kind='bar')
+        graph_path = os.path.join(STATIC_FOLDER, 'graph.png')
+        plt.savefig(graph_path)
+
+        return redirect(url_for('show_graph'))
+
+
+@app.route('/graph')
+def show_graph():
+    # Displaying the graph
+    return '<img src="/static/graph.png" alt="Graph">'
 
 
 if __name__ == '__main__':
